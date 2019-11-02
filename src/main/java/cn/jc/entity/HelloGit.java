753101e08d87e74dd3427ea3506a07ba2683919f
EquationPro <47234290+EquationPro@users.noreply.github.com>
2019-11-02 11:03:43 +0800

    Update HelloGit.java

--- src/main/java/cn/jc/entity/HelloGit.java
+++ src/main/java/cn/jc/entity/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("update1");
+        System.out.println("hello cloud");
     }
 }
