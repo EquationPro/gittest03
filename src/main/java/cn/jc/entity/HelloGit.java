1f2bd34387aa468eb7eefce547522ee11a7d603a
user007 <22589675962@qq.com>
2019-11-02 10:54:09 +0800

    Initial commit

---
+++ src/main/java/cn/jc/entity/HelloGit.java
@@ -0,0 +1,11 @@
+package cn.jc.entity;
+
+/**
+ * @author equation
+ * @date 2019/11/2
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
